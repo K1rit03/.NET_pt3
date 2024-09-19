4ee94558320144a3251c165604e75355efb987a3
Laboratório FIAP <labsfiap@fiap.com.br>
2024-09-19 11:12:28 -0300

    adicionando validação na propriedade nome

--- Models/Pessoa.cs
+++ Models/Pessoa.cs
@@ -1,17 +1,32 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace ExemploExplorando.Models
 {
     public class Pessoa
     {
-     public string Nome { get; set; }
-     public int Idade { get; set; } 
+        private string _nome; 
 
-     public void Apresentar(){
-        Console.WriteLine($"Nome: {Nome}, idade: {Idade}");
-     }  
+        public string Nome 
+        { 
+            get
+            {
+                return _nome.ToUpper();
+            } 
+            set 
+            {
+                if (value == "") 
+                {
+                    throw new ArgumentException("O nome não pode ser vazio");
+                }
+                _nome = value; // 
+            } 
+        }
+
+        public int Idade { get; set; } 
+
+        public void Apresentar()
+        {
+            Console.WriteLine($"Nome: {Nome}, idade: {Idade}");
+        }  
     }
 }
